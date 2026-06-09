1f243b7a7df76262fccb162d7757a449a2b6e578
sachinkr1530 <royalsachin1530@gmail.com>
2026-06-09 22:46:57 +0530

    fix api base url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+// const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
+const API_BASE = process.env.REACT_APP_API_URL || 'https://finance-management-bwm8.onrender.com/api';
+
 
 const api = axios.create({
   baseURL: API_BASE,
